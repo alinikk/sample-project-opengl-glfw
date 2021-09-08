2876c7a2f0948cd4eebd66b44a89c46cee1d8be4
Ali Nikkhah <stu209880@mail.uni-kiel.de>
2021-09-08 15:00:16 +0200

    added libraries

--- Sample Engine/Source/Main.cpp
+++ Sample Engine/Source/Main.cpp
@@ -1,3 +1,6 @@
+#include <stdio.h>
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -6,7 +9,7 @@ int main(void)
 
   /* Initialize the library */
   if (!glfwInit())
-    return -1;
+    return -1; 
 
   /* Create a windowed mode window and its OpenGL context */
   window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -19,11 +22,36 @@ int main(void)
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
 
+  GLenum err = glewInit();
+  if (err != GLEW_OK)
+  {
+    fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+  }
+
+  printf("%s\n", glGetString(GL_VERSION));
+
+  /* Outside the loop */
+
+  GLfloat vertex_positions[6] =
+    { -0.5f, -0.5f,
+       0.0f,  0.5f,
+       0.5f, -0.5f };
+
+  GLuint buffer;
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(GLfloat), &vertex_positions, GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0);
+
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window))
   {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
